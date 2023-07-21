d62ea699ed2148a1690730cd321d59756ffd639f
Sakib Pathen <107497530+srpathen@users.noreply.github.com>
2023-07-21 18:17:51 -0400

    DraftKings Rushing Odds Error

--- RushingOdds.sh
+++ RushingOdds.sh
@@ -1,6 +1,6 @@
-python NFL/RushingOdds/Code/FanDuel.py
+#python NFL/RushingOdds/Code/FanDuel.py
 python NFL/RushingOdds/Code/DraftKings.py
-python NFL/RushingOdds/Code/BetMGM.py
-python NFL/RushingOdds/Code/Caesars.py
+#python NFL/RushingOdds/Code/BetMGM.py
+#python NFL/RushingOdds/Code/Caesars.py
 
 
